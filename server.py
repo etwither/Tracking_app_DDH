5722763d230e954690c900764bab5c289a39cec2
etwither <66630783+etwither@users.noreply.github.com>
2023-04-25 14:42:22 -0500

    Initial commit
    Runs the web server to host the SQLite database.  Allows GET and POST request to receive and send location data.

---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, g, request, jsonify
+import sqlite3
+import subprocess
+
+# Run the script using subprocess
+subprocess.call(["python3", "startdb.py"])
+
+app = Flask(__name__)
+
+#python3 startdb.py
+DATABASE = 'location.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_db(error):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+        
+@app.route('/')
+def index():
+    db = get_db()
+    cur = db.execute('SELECT * FROM Position')
+    rows = cur.fetchall()
+    return str(rows)
+    
+@app.route('/add_data', methods=['GET'])
+def add_data():
+    ids = request.args.get('Id')
+    lat = request.args.get('Lat')
+    log = request.args.get('Log')
+    db = get_db()
+    db.execute('INSERT INTO Position (Id, Lat, Log) VALUES (?, ?, ?)', (ids, lat, log))
+    db.commit()
+    return 'Data added to database'
+    
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    data = []
+    db = get_db()
+    cur = db.execute('SELECT * FROM Position')
+    rows = cur.fetchall()
+    for row in rows:
+        data.append(dict(row))
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
+
